ecc84aa8880208eb9e94707782bd22ef9ef129f4
Pedro <pedrofreitas790@gmail.com>
2021-03-27 15:12:44 -0300

    Pasta removida.

--- jymbo-api/src/database/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('db_jymbo', 'root', 'Goncomeuocabo2020!', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
-
-module.exports = sequelize;
